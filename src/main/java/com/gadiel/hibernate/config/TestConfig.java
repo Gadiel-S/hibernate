049868b8784d3cb69977ffb6c8b028f52d2ab7f1
Gadiel-S <gadielbarrosdesouza@gmail.com>
2025-02-02 20:36:08 -0300

    Entidade Payment e associação um para um

--- src/main/java/com/gadiel/hibernate/config/TestConfig.java
+++ src/main/java/com/gadiel/hibernate/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.gadiel.hibernate.entities.Category;
 import com.gadiel.hibernate.entities.Order;
 import com.gadiel.hibernate.entities.OrderItem;
+import com.gadiel.hibernate.entities.Payment;
 import com.gadiel.hibernate.entities.Product;
 import com.gadiel.hibernate.entities.User;
 import com.gadiel.hibernate.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
